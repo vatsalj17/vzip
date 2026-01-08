45462451e44f4d4eb7ffc499086c759bb6f69eee
Vatsalj17 <vatsaljaiswal17@gmail.com>
2026-01-08 20:06:18 +0530

    tree deformity

--- huffman.c
+++ huffman.c
@@ -1,11 +1,39 @@
 #include "huffman.h"
 
-#include "tree.h"
+#include <stdlib.h>
+#include "heap.h"
 
-typedef struct HuffCode {
-	uint32_t bits;
-	uint8_t length;
-} huff_code;
+typedef struct Node {
+	unsigned char symbol;
+	uint64_t freq;
+	struct Node* left;
+	struct Node* right;
+} node_t;
+
+node_t* node_init(unsigned char symbol, uint64_t frequency) {
+	node_t* new = malloc(sizeof(node_t));
+	new->freq = frequency;
+	new->symbol = symbol;
+	new->left = NULL;
+	new->right = NULL;
+	return new;
+}
+
+int node_comparator(const void* a, const void* b) {
+	// for min heap
+	node_t* na = (node_t*)a;
+	node_t* nb = (node_t*)b;
+	if (na->freq > nb->freq)
+		return -1;
+	else if (na->freq < nb->freq)
+		return 1;
+	return 0;
+}
+
+bool is_leaf(node_t* n) {
+    if (n->left == NULL && n->right == NULL) return true;
+    return false;
+}
 
 void build_frequency_table(FILE* file, uint64_t freq[256]) {
 	unsigned char c;
@@ -14,5 +42,46 @@ void build_frequency_table(FILE* file, uint64_t freq[256]) {
 	}
 }
 
+node_t* build_huffman_tree(uint64_t freq[256]) {
+	heap_t* heap = heap_init(256, node_comparator);
+	// put every thing in heap
+	for (int i = 0; i < 256; i++) {
+		if (freq[i] > 0) {
+			node_t* node = node_init(i, freq[i]);
+			heap_push(heap, node);
+		}
+	}
+
+	// build tree
+	while (!is_size_one(heap)) {
+		node_t* left = heap_pop(heap);
+		node_t* right = heap_pop(heap);
+		node_t* top = node_init(0, left->freq + right->freq);
+		top->left = left;
+		top->right = right;
+		heap_push(heap, top);
+        printtree(heap_top(heap), 0);
+	}
+	return heap_top(heap);
+}
+
+void printtree(node_t* root, int level) {
+    if (!root) return;
+    for (int i = 0; i < level; i++) printf("  ");
+    printf("\'%d:%c %lu\' \n", root->symbol, root->symbol, root->freq);
+    printtree(root->left, level + 1);
+    printtree(root->right, level + 1);
+}
+
 void build_code_table(node_t* root, huff_code table[256], uint32_t code, int level) {
+    if (!root) return;
+    if (is_leaf(root)) {
+        table[root->symbol].bits = code;
+        table[root->symbol].length = level;
+        return;
+    }
+    // left => 0
+    build_code_table(root->left, table, code << 1, level + 1);
+    // right => 1
+    build_code_table(root->right, table, (code << 1) | 1, level + 1);
 }
--- huffman.h
+++ huffman.h
@@ -3,7 +3,22 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+typedef struct Node  node_t;
+
+typedef struct HuffCode {
+	uint32_t bits;
+	uint8_t length;
+} huff_code;
+
+node_t* node_init(unsigned char symbol, uint64_t frequency);
+int node_comparator(const void* a, const void* b);
+node_t* build_huffman_tree(uint64_t freq[256]);
+bool is_leaf(node_t* n);
+void printtree(node_t* root, int level);
 
 void build_frequency_table(FILE* file, uint64_t freq[256]);
+void build_code_table(node_t* root, huff_code table[256], uint32_t code, int level);
 
 #endif
